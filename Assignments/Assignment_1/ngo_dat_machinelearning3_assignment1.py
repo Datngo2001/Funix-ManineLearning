c0afca6dc6a363a4e965d14304b32985f9a90d34
datngo2001 <ngominhdat110115@gmail.com>
2024-04-02 00:20:24 +0700

    Assignment 1: Task 5

--- Assignments/Assignment_1/ngo_dat_machinelearning3_assignment1.py
+++ Assignments/Assignment_1/ngo_dat_machinelearning3_assignment1.py
@@ -1,15 +1,13 @@
 
 # Task 1: Input file name
-
+import pandas as pd
 
 BASE_DATA_FOLDER = f'data-files\\Data_Files'
 
 class_file_name = input("Enter a class file to grade (i.e. class1 for class1.txt): ")
-file_content = ''
 
-try:
-    with open(f'{BASE_DATA_FOLDER}\\{class_file_name}.txt', 'r') as file:
-        file_content = file.read()
+try:        
+    class_df = pd.read_csv(f'{BASE_DATA_FOLDER}\\{class_file_name}.txt', index_col=0, header=None)
     print(f'Successfully opened {class_file_name}.txt')
 except IOError:
     print('File cannot be found.')
@@ -22,7 +20,7 @@ import re
 REQUIRED_ANSWER = 25
 
 errors = []
-valid_rows = []
+valid_df = pd.DataFrame(columns = class_df.columns)
 
 class ErrorType(Enum):
     ID_INVALID = 1
@@ -41,25 +39,19 @@ class DataError:
 
 print('\n**** ANALYZING ****\n')
 
-rows = file_content.split('\n')
-
-for row in rows:
-    columns = row.split(',')
+for index, row in class_df.iterrows():
     
-    if(len(columns) != REQUIRED_ANSWER + 1 ):
-        
+    if(len(row) != REQUIRED_ANSWER):
         error = DataError(
             type = ErrorType.WRONG_ANSWER_AMOUNT,
             message = f'Invalid line of data: does not contain exactly {REQUIRED_ANSWER + 1} values:',
             data = row,
         )
-        
         error.print()
         errors.append(error)
-        
         continue
     
-    student_id = columns[0]
+    student_id = index
     
     if(not re.match(r'^N\d{8}$', student_id)):
         error = DataError(
@@ -67,37 +59,35 @@ for row in rows:
             message = f'Invalid line of data: N# is invalid',
             data = row,
         )
-        
         error.print()
         errors.append(error)
-        
         continue
     
-    valid_rows.append(row)
+    valid_df.loc[index] = row
         
 print('\n**** REPORT ****\n')
 
-print(f'Total valid lines of data: {len(rows) - len(errors)}')
+print(f'Total valid lines of data: {len(class_df) - len(errors)}')
 
-print(f'Total invalid lines of data: {len(errors)}')
+print(f'Total invalid lines of data: {len(errors)} \n')
 
 # Task 3: Calculate
 import numpy as np
 
-ANSWER_KEYS = ("B,A,D,D,C,B,D,A,C,C,D,B,A,B,A,C,B,D,A,C,A,A,B,D,D").split(',')
+ANSWER_KEYS = pd.Series(("B,A,D,D,C,B,D,A,C,C,D,B,A,B,A,C,B,D,A,C,A,A,B,D,D").split(','))
 
 student_scores = []
 
-for row in valid_rows:
-    row_split = str(row).split(',')
-    student_id = row_split[0]
-    student_answers = row_split[1:]
+for index, row in valid_df.iterrows():
+    student_id = index
+    student_answers = row
     student_score = 0
     
-    for i in range(0, len(student_answers) - 1):
-        if(len(student_answers[i]) == 0):
+    for i, answer in student_answers.items():
+        if(pd.notna(answer) == False):
             continue
-        elif(student_answers[i] == ANSWER_KEYS[i]):
+        elif(answer == ANSWER_KEYS[i-1]):
+            temp = ANSWER_KEYS[i-1]
             student_score += 4
         else:
             student_score += -1
@@ -113,3 +103,8 @@ print(f'Highest score: {scores.max()}')
 print(f'Lowest score: {scores.min()}')
 print(f'Range of scores: {scores.max() - scores.min()}')
 print(f'Median score: {np.median(scores)}')
+
+# Task 4: Save Result
+
+df = pd.DataFrame(student_scores)
+df.to_csv(f'results/{class_file_name}_grades.txt', index=False, header=False)
